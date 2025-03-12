42455b39857bf3e8c80a0b5a6fea45870dee5dbb
easttwave <hd.lee6@cj.net>
2025-03-12 17:03:32 +0900

    config server client test 작성

--- cloud-config-client/src/main/java/demo/cloudconfigclient/CloudConfigClientApplication.java
+++ cloud-config-client/src/main/java/demo/cloudconfigclient/CloudConfigClientApplication.java
@@ -2,8 +2,10 @@ package demo.cloudconfigclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @SpringBootApplication
+@ConfigurationProperties("com.easttwave")
 public class CloudConfigClientApplication {
 
     public static void main(String[] args) {
--- cloud-config-client/src/test/java/demo/cloudconfigclient/CloudConfigClientApplicationTests.java
+++ cloud-config-client/src/test/java/demo/cloudconfigclient/CloudConfigClientApplicationTests.java
@@ -1,13 +1,21 @@
 package demo.cloudconfigclient;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class CloudConfigClientApplicationTests {
 
+    @Autowired
+    private MyConfig myConfig;
+
     @Test
     void contextLoads() {
+        assertThat(myConfig.getProfile()).isEqualTo("demo");
     }
+
 
 }
